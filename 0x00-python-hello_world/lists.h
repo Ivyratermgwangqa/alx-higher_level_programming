92a15b18326459d0af0af7c8733a5fbf68019dd4
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-08-09 18:10:47 -0700

    update

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -3,13 +3,14 @@
 
 #include <stdlib.h>
 
+
 /**
- * struct listint_s - singly linked list node
- * @n: The integer stored in the node
- * @next: Points to the next node in the list
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Description: Represents a node in a singly linked list
- * for a Holberton project.
+ * Description: singly linked list node structure
+ * 
  */
 typedef struct listint_s
 {
@@ -22,4 +23,4 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif
+#endif /* LISTS_H */
