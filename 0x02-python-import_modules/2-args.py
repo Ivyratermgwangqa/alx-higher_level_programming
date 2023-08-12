0ec7950f21170bb17b2c3811ebae0b9106858c3f
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-08-11 18:32:28 -0700

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,5 @@ if len(argv) > 1:
     print(f"{len(argv)} arguments:")
     for i in range(1, len(argv)):
         print(f"{i}: {argv[i]}")
-else:
-    print(f"0 arguments.")
-
+    else:
+        print(f"0 arguments.")
