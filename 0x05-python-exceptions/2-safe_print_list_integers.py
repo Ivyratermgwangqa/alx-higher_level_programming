c4003ee98a6d7c458445d2c53a0645740837c328
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-08-28 19:02:27 -0700

    update

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    num_integers = 0  # Changed variable name
+    num_integers = 0
     
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            num_integers = num_integers + 1  # Changed variable name
+            num_integers = num_integers + 1
         except (ValueError, TypeError):
             continue
     print("")
