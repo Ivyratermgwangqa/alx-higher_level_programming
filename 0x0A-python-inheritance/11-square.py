4d462285d452f8223bfad84f77b65f6c02dfd511
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-09-12 12:19:57 -0700

    update

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""MyInt Module
+This module defines the MyInt class, which inherits from the int type.
+MyInt overrides the equality and inequality operators to provide custom behavior.
+
+"""
+
+class MyInt(int):
+    """Type class of MyInt inherit int type"""
+
+    def __eq__(self, other):
+        return self.real != other
+
+    def __ne__(self, other):
+        return self.real == other
