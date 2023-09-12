3c7ea6927845d41e396935d3c6be13511a60f450
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-09-12 12:06:06 -0700

    updating documentation

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def inherits_from(obj, a_class):
+    """Function to check if an object is inherited in a class.
+    Args:
+    param1: obj
+        The object to be checked.
+    param2: a_class
+        The class to check if 'obj' is inherited from.
+
+    Returns:
+    True if 'obj' is a subclass of 'a_class', otherwise False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    else:
+        return False
