b81e3850e97376867679cdc5a10a8a9bc8a4eedf
Ivyratermgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2023-09-13 21:51:16 +0200

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Module: my_module
+This module contains a function for looking up the attributes and methods of an object.
+"""
 class MyList(list):
     """Type class MyList  inherits print_sorted function"""
 
