8449a2910d33ab8706ad8a5510ee5af1d85a2a23
Ivyratermgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2023-09-13 23:58:40 +0200

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
 Module: my_module
-This module contains a function for looking up the attributes and methods of an object.
+This module contains a function for looking up the attributes and methods.
 """
 def lookup(obj):
-    """Return list of obj"""
+    """
+    Return list of obj
+    """
     return (dir(obj))
