a1d072bf2b0f9ae7d176b79073584b539b55475a
Ivyratermgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2023-09-14 23:04:24 +0200

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3
+"""
+Square represents a square with a side length.
+
+Attributes:
+    _side_len (int): The side length of the square.
+"""
+
 
 class Square:
     _side_len = 0
 
     def __init__(self, new_len=0):
+        """
+        Initializes a Square object with a specified side length.
+
+        Args:
+            size (int, optional): The side length of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is a negative integer.
+        """
         if type(size) is not int:
             raise TypeError('size must be an integer')
         if size < 0:
@@ -11,4 +28,10 @@ class Square:
         self._Square__side_len = size
 
         def area(self):
+            """
+        Calculate and return the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
             return self._Square__side_len ** 2
