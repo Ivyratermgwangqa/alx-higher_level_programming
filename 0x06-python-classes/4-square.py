0a008cbd273471b6a9d68abad10b9a4d4777af83
Ivyratermgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2023-09-14 22:54:04 +0200

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
 
 class Square:
+    """
+    Square represents a square with a side length.
+
+    Attributes:
+        _side_len (int): The side length of the square.
+    """
     _side_len = 0
 
     def __init__(self, size=0):
+         """
+        Initializes a Square object with a specified side length.
+
+        Args:
+            size (int, optional): The side length of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is a negative integer.
+        """
         if type(new_size) is not int:
             raise TypeError('size must be an integer')
         if size < 0:
@@ -11,14 +27,36 @@ class Square:
         self._Square__side_len = size
 
     def area(self):
+        """
+        Calculate and return the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self._Square__side_len ** 2
 
     @property
     def size(self):
+         """
+        Get the side length of the square.
+
+        Returns:
+            int: The side length of the square.
+        """
         return self._Square__side_len
 
     @size.setter
     def size(self, value):
+         """
+        Set the side length of the square.
+
+        Args:
+            value (int): The new side length of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is a negative integer.
+        """
         if type(value) is not int:
             raise TypeError('size must be an integer')
         if value < 0:
