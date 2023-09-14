e807cacbd1b0a9db409ab383cc8fa70eb765fc19
Ivyratermgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2023-09-14 22:43:28 +0200

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This is the module-level documentation for the square module.
+
+The module contains the Square class, which is an empty class.
+"""
+
 
 class Square:
     """This is an empty class named Square."""
