20f42f6a647f2a7b9c199541ff29c0b59e448aaf
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2023-09-23 01:36:05 +0200

    Update test_base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,8 +1,12 @@
+#!/usr/bin/python3
+"""Unittest for Base module"""
+
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 import os
+
 
 class TestBaseInstances(unittest.TestCase):
 
