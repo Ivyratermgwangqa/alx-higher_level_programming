5d22c82486aa9fa513965b8088441baec0e1eb3a
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2023-10-03 11:06:31 +0200

    Delete 0x09-python-everything_is_object directory

--- 0x09-python-everything_is_object/100-magic_string.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def magic_string(number=0):
-    magic_string.number = getattr(magic_string, "number", 0) + 1
-    return (("Holberton, " * magic_string.number)[:-2])
--- 0x09-python-everything_is_object/101-locked_class.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-class LockedClass:
-    __space__ = ["first_name"]
