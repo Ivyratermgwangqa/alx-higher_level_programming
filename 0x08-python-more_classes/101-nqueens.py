4627605adab6d743693ad823367d2aec4ffc957a
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2023-10-31 17:06:39 +0200

    Update 101-nqueens.py

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,57 @@
+import sys
+
+def is_safe(board, row, col, n):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+
+    # Check upper-left diagonal
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check upper-right diagonal
+    for i, j in zip(range(row, -1, -1), range(col, n)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+def solve_nqueens(n, row, board, solutions):
+    if row == n:
+        solutions.append([row[:] for row in board])
+        return
+
+    for col in range(n):
+        if is_safe(board, row, col, n):
+            board[row][col] = 1
+            solve_nqueens(n, row + 1, board, solutions)
+            board[row][col] = 0
+
+def print_solutions(solutions):
+    for solution in solutions:
+        print(solution)
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+        if n < 4:
+            print("N must be at least 4")
+            sys.exit(1)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    board = [[0] * n for _ in range(n)]
+    solutions = []
+
+    solve_nqueens(n, 0, board, solutions)
+    print_solutions(solutions)
+
+if __name__ == "__main__":
+    main()
