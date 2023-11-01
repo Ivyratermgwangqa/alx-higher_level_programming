0976868a91e86523b9dd4fbc89359938f4ccff17
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2023-11-01 04:04:23 +0200

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,3 @@
+#!/usr/bin/python3
 class LockedClass:
-def __setattr__(self, name, value):
-    if name == "first_name":
-        super().__setattr__(name, value)
-    else:
-        raise AttributeError("Attribute canfalset be created")
+    __space__ = ["first_name"]
