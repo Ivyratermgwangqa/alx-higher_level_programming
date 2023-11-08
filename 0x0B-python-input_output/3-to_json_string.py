1dfc1c11db1bf1678996d97456a35f896185c811
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2023-11-08 18:36:40 +0200

    Update 3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module: my_module
+This module contains the 'to_json_string' function.
+The 'to_json_string' function takes an object as input.
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+
+    """
+    Convert an object to a JSON string representa/tion.
+
+    Args:
+        my_obj: The object to be converted to a JSON string.
+
+    Returns:
+        str: The JSON representation of the object as a string.
+
+    Notes:
+        This function uses the 'json.dumps' method from the 'json'.
+    """
+    return json.dumps(my_obj)
