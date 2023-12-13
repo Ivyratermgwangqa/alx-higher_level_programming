ea330c64cdadcb763b5c8e8c854faf012a3fa911
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2023-12-13 04:36:00 +0200

    Update 103-max_state.sql

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,6 +1,2 @@
 -- Calculate the max temperature for each state
-USE hbtn_0c_0;
-SELECT state, MAX(temp) as max_temp
-FROM temperatures
-GROUP BY state
-ORDER BY state;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
