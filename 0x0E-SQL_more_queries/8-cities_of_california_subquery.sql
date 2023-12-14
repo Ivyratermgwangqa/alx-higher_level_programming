d37f5d087730e98653512377e057d47aeb4c0992
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2023-12-14 03:53:03 +0200

    Update 8-cities_of_california_subquery.sql

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- Lists all cities of California from the database hbtn_0d_usa.
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = states.id AND states.name = 'California'
+ORDER BY cities.id;
