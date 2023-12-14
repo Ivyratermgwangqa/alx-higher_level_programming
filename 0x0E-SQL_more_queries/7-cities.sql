ddeea8dc793b8981e730a8acd0af999e4e87e405
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2023-12-14 04:10:12 +0200

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,8 @@
 -- Creates the database hbtn_0d_usa and the table cities within it.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id)
-);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+  PRIMARY KEY(id), id INT NOT NULL AUTO_INCREMENT,
+  state_id INT NOT NULL,
+  name VARCHAR(256) NOT NULL,
+  FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
+    );
