9a963dda9ddb7de5bb377adc09e14372a69d489b
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2023-12-14 03:30:47 +0200

    Update 13-count_shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Lists all genres from hbtn_0d_tvshows along with the number of linked shows for each genre.
+USE hbtn_0d_tvshows;
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
