6514d639831c7dd5d609302f93c9d8ef4e638310
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2023-12-14 04:29:46 +0200

    Update 102-rating_shows.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- Lists all shows from the hbtn_0d_tvshows_rate database by their ratings.
-SELECT tv_shows.title, SUM(rating) AS rating_sum
+-- List all shows from hbtn_0d_tvshows_rate by their rating
+SELECT title, SUM(rating) as rating_sum
 FROM tv_shows
-JOIN tvshows_rate ON tv_shows.id = tvshows_rate.show_id
-GROUP BY tv_shows.title
+GROUP BY title
 ORDER BY rating_sum DESC;
