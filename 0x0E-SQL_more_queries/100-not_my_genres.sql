7e3907488953d5554b6481795cc00bba11321e07
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2023-12-14 03:24:25 +0200

    Update 100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,10 +1,10 @@
--- Lists all shows in the hbtn_0d_tvshows database without the Comedy genre.
-SELECT tv_shows.title
-FROM tv_shows
-WHERE tv_shows.id NOT IN (
-    SELECT tv_show_genres.show_id
+-- Lists all genres not linked to the show "Dexter" in the hbtn_0d_tvshows database.
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.id NOT IN (
+    SELECT tv_show_genres.genre_id
     FROM tv_show_genres
-    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-    WHERE tv_genres.name = 'Comedy'
+    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title = 'Dexter'
 )
-ORDER BY tv_shows.title;
+ORDER BY tv_genres.name;
