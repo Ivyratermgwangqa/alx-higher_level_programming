19366d433e66dd5cf0e36c40b61c13118b7e5b19
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-01-09 03:32:27 +0200

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
-exports.add = function (a, b) {
-  return a + b;
-};
+
+function add(a, b) {
+  return parseInt(a) + parseInt(b);
+}
+
+const num1 = process.argv[2];
+const num2 = process.argv[3];
+
+console.log(add(num1, num2));
