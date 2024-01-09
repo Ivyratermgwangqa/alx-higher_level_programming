c73b042525f7b5c5651c4056e81e96040a1fbfb2
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-01-09 02:55:51 +0200

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-const numArgs = args.length;
+const argsCount = process.argv.length - 2;
 
-if (numArgs === 0) {
-  console.log("No argument");
-} else if (numArgs === 1) {
-  console.log("Argument found");
+if (argsCount === 0) {
+  console.log('No argument');
+} else if (argsCount === 1) {
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
