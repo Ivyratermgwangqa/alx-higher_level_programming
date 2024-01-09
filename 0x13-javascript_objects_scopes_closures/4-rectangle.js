4310d30fb203cacaab0a8944e93f4bf5d61fadc6
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-01-10 00:25:42 +0200

    Update 4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+// 4-rectangle.js
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      this.width = undefined;
+      this.height = undefined;
+    }
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate() {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
