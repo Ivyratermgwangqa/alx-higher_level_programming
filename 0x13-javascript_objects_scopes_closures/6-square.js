5aac6aee04fb847e5228f1bce968c696b98d1caa
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-01-10 00:26:30 +0200

    Update 6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+// 6-square.js
+const Square = require('./5-square');
+
+class Square extends Square {
+  constructor(size) {
+    super(size);
+  }
+
+  charPrint(c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
