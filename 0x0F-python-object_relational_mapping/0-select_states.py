7a16e26ce5c9ad86f94652430127c5320ce8045b
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-01-13 03:24:29 +0200

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all states with a name starting with N from the database hbtn_0e_0_usa
+Lists all states from the database hbtn_0e_0_usa
 """
 import sys
 import MySQLdb
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     )
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    cursor.execute("SELECT * FROM states ORDER BY id")
     states = cursor.fetchall()
 
     for state in states:
