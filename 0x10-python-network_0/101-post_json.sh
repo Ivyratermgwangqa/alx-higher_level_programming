92a678d138b70149d44fad75c8b809d23f561dfe
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-01-26 10:20:35 +0200

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# Sends a JSON POST request to a URL and displays the body of the response
-filename=$2
-if [ -f "$filename" ]; then
-    curl -sX POST -H "Content-Type: application/json" -d @"$filename" "$1"
-else
-    echo "Not a valid JSON"
-fi
+# Script that sends a JSON POST request to a URL with the contents of a file
+curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
